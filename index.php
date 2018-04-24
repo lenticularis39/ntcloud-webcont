d1c1abf05b15aa3b1dbaef36a058fce05037ce10
Pavel Kladivák <tglozar@gmail.com>
2018-04-24 18:43:05 +0200

    Fix.

--- index.php
+++ index.php
@@ -36,7 +36,6 @@
 // +--------------------------------------------------
 // | Header and Globals
 // +--------------------------------------------------
-error_reporting(E_ERROR | E_PARSE);
 if(!isset($_SERVER['PATH_INFO']) && isset($_SERVER["ORIG_PATH_INFO"])) {
     $_SERVER["PATH_INFO"] = $_SERVER["ORIG_PATH_INFO"];
 }
@@ -5936,4 +5935,33 @@ function et($tag){
     $et['ca']['NoReturn'] = 'sense retorn';
     $et['ca']['FileSent'] = 'Arxiu enviat';
     $et['ca']['SpaceLimReached'] = 'Límit d`espaci al disc assolit';
-    
+    $et['ca']['InvExt'] = 'Extensió no vàlida';
+    $et['ca']['FileNoOverw'] = 'L`arxiu no ha pogut ser sobreescrit';
+    $et['ca']['FileOverw'] = 'Arxiu sobreescrit';
+    $et['ca']['FileIgnored'] = 'Arxiu ignorat';
+    $et['ca']['ChkVer'] = 'Revisar les actualitzacions';
+    $et['ca']['ChkVerAvailable'] = 'Nova versió, feu clic aquí per descarregar';
+    $et['ca']['ChkVerNotAvailable'] = 'La vostra versió és la més recent.';
+    $et['ca']['ChkVerError'] = 'Error de connexió.';
+    $et['ca']['Website'] = 'Lloc Web';
+    $et['ca']['SendingForm'] = 'Enviant arxius, esperi';
+    $et['ca']['NoFileSel'] = 'Cap arxiu seleccionat';
+    $et['ca']['SelAll'] = 'Tots';
+    $et['ca']['SelNone'] = 'Cap';
+    $et['ca']['SelInverse'] = 'Invers';
+    $et['ca']['Selected_s'] = 'seleccionat';
+    $et['ca']['Total'] = 'total';
+    $et['ca']['Partition'] = 'Partició';
+    $et['ca']['RenderTime'] = 'Generat en';
+    $et['ca']['Seconds'] = 'seg';
+    $et['ca']['ErrorReport'] = 'Informe d`error';
+
+    if (!strlen($lang)) $lang = $sys_lang;
+    if (isset($et[$lang][$tag])) return html_encode($et[$lang][$tag]);
+    else if (isset($et['en'][$tag])) return html_encode($et['en'][$tag]);
+    else return "[$tag]"; // So we can know what is missing
+}
+// +--------------------------------------------------
+// | THE END
+// +--------------------------------------------------
+?>
