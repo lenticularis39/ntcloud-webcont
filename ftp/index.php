991e0d68d61bba16749a5cbb18ee6b9d06204759
Pavel Kladivák <tglozar@gmail.com>
2018-04-25 17:53:25 +0200

    New FTP server.

--- ftp/index.php
+++ ftp/index.php
@@ -1,152 +1,76 @@
 <?php
-    require_once(dirname(__FILE__) . "/application/api/constants.php");
-    includeMonstaConfig();
-    require_once(dirname(__FILE__) . '/application/api/lib/helpers.php');
-    require_once(dirname(__FILE__) . '/application/api/lib/entry_handlers.php');
-    if (file_exists(dirname(__FILE__) . '/mftp_extensions.php'))
-        include_once(dirname(__FILE__) . '/mftp_extensions.php');
 
-    require_once(dirname(__FILE__) . '/application/api/system/ApplicationSettings.php');
-
-    $applicationSettings = new ApplicationSettings(APPLICATION_SETTINGS_PATH);
-
-    $languageDir = dirname(__FILE__) . "/application/languages/";
-
-    $languages = readLanguagesFromDirectory($languageDir);
-
-    $license = readDefaultMonstaLicense();
-    $isLicensed = !is_null($license) && $license->isLicensed();
-    $isEnterpriseEdition = $isLicensed && $license->getLicenseVersion() >= 3 && $license->isMonstaEnterpriseEdition();
-    $isPostEntry = $isEnterpriseEdition && isMonstaPostEntry($_SERVER['REQUEST_METHOD'], $_POST);
-
-    $versionQS = generateVersionQueryString($isLicensed, $isEnterpriseEdition);
-
-    $resetPasswordAvailable = false;
-    $forgotPasswordAvailable = false;
-
-    if ($isEnterpriseEdition) {
-        if (function_exists("mftpInitialLoadValidation")) {
-            if (!mftpInitialLoadValidation($isPostEntry))
-                exit();
-        }
-
-        $resetPasswordAvailable = function_exists("mftpResetPasswordHandler");
-        $forgotPasswordAvailable = function_exists("mftpForgotPasswordHandler");
-    }
-?>
-<!DOCTYPE html>
-<html ng-app="MonstaFTP">
-<head>
-    <title><?php print getMonstaPageTitle($isEnterpriseEdition); ?></title>
-
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
-    <link rel="shortcut icon" type="image/x-icon" href="application/frontend/images/logo-favicon.png">
-    <link rel="apple-touch-icon" href="application/frontend/images/logo-webclip.png">
-
-    <script>
-        var g_defaultLanguage = "<?php print $applicationSettings->getLanguage(); ?>";
-        var g_upgradeURL = "http://www.monstaftp.com/upgrade";
-        var g_loadComplete = false;
-        var g_xhrTimeoutSeconds = <?php print $applicationSettings->getXhrTimeoutSeconds(); ?>;
-        var g_isMonstaPostEntry = false;
-        var g_isNewWindowsInstall = <?php print booleanToJsValue(getNormalizedOSName() == "Windows" && !$isLicensed); ?>;
-        var g_ftpConnectionAvailable = <?php print booleanToJsValue(ftpConnectionAvailable()); ?>;
-        var g_openSslAvailable = <?php print booleanToJsValue(function_exists("openssl_get_publickey")); ?>;
-        var g_resetPasswordAvailable = <?php print booleanToJsValue($resetPasswordAvailable); ?>;
-        var g_forgotPasswordAvailable = <?php print booleanToJsValue($forgotPasswordAvailable); ?>;
-
-        <?php
-        if ($isLicensed && $isPostEntry) {
-        ?>
-        g_isMonstaPostEntry = true;
-        var g_monstaPostEntryVars = <?php print json_encode(extractMonstaPostEntryVars($_POST)); ?>;
-        <?php
-        }
-        ?>
-    </script>
-
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,600,300">
-    <script src="application/frontend/assets-<?php print MONSTA_VERSION; ?>/vendor.js"></script>
-
-    <link rel="stylesheet" href="application/frontend/css/monsta.css">
-    <link rel="stylesheet" href="settings/theme.css">
+/*  Hi,
     
-    <script src="application/frontend/js/monsta-min-<?php print MONSTA_VERSION; ?>.js"></script>
-    <script src="application/frontend/js/templates-<?php print MONSTA_VERSION; ?>.js"></script>
+    Thanks for downloading net2ftp!
+    
+    This page shows how to integrate net2ftp in a generic PHP page.
+    It is quite easy:
+    1. Define the constants NET2FTP_APPLICATION_ROOTDIR and NET2FTP_APPLICATION_ROOTDIR_URL
+    2. Include the file main.inc.php
+    3. Execute 5 net2ftp() calls to send the HTTP headers, print the Javascript 
+       code, print the HTML body, etc...
+    4. Check if an error occured to print out an error message.
+    
+    Look in /integration for more elaborate examples.
+    
+    Enjoy,
+    
+    David 
+*/
 
-    <?php
-        if ($applicationSettings->isSettingsReadFailed()) {
-            ?>
-            <script>
-                var g_settingsReadFailureMesage = "Reading settings.json failed. <?php echo $applicationSettings->getSettingsReadError()?>.\n\nCheck the file is readable and " +
-                    "has no syntax errors (http://jsonlint.com might help). You are using the default settings.";
+// ------------------------------------------------------------------------
+// 1. Define the constants NET2FTP_APPLICATION_ROOTDIR and NET2FTP_APPLICATION_ROOTDIR_URL
+// ------------------------------------------------------------------------
+$http_scheme = "http://";
+if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") { $http_scheme = "https://"; }
+$http_host = $_SERVER["HTTP_HOST"];
+if     (isset($_SERVER["SCRIPT_NAME"]) == true)  { $http_directory = dirname($_SERVER["SCRIPT_NAME"]); }
+elseif (isset($_SERVER["PHP_SELF"]) == true)     { $http_directory = dirname($_SERVER["PHP_SELF"]); }
+if ($http_directory == "/") { $http_directory = ""; }
+define("NET2FTP_APPLICATION_ROOTDIR", dirname(__FILE__));
+define("NET2FTP_APPLICATION_ROOTDIR_URL", $http_scheme . $http_host . $http_directory);
 
-                alert(g_settingsReadFailureMesage);
-            </script>
-            <?php
-        }
-    ?>
-    <script>
-        var g_languageFiles = <?php print json_encode($languages); ?>;
-    </script>
+// ------------------------------------------------------------------------
+// 2. Include the file /path/to/net2ftp/includes/main.inc.php
+// ------------------------------------------------------------------------
+require_once("./includes/main.inc.php");
+
+// ------------------------------------------------------------------------
+// 3. Execute net2ftp($action). Note that net2ftp("sendHttpHeaders") MUST 
+//    be called once before the other net2ftp() calls!
+// ------------------------------------------------------------------------
+net2ftp("sendHttpHeaders");
+if ($net2ftp_result["success"] == false) {
+	require_once("./skins/shinra/error_wrapped.template.php");
+	exit();
+}
+?>
+<!DOCTYPE html PUBLIC "XHTML 1.0 Transitional" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang="<?php echo __("en"); ?>" dir="<?php echo __("ltr"); ?>">
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=<?php echo __("iso-8859-1"); ?>" />
+<meta name="keywords" content="net2ftp, web, ftp, based, web-based, xftp, client, PHP, SSL, SSH, SSH2, password, server, free, gnu, gpl, gnu/gpl, net, net to ftp, netftp, connect, user, gui, interface, web2ftp, edit, editor, online, code, php, upload, download, copy, move, delete, zip, tar, unzip, untar, recursive, rename, chmod, syntax, highlighting, host, hosting, ISP, webserver, plan, bandwidth" />
+<meta name="description" content="net2ftp is a web based FTP client. It is mainly aimed at managing websites using a browser. Edit code, upload/download files, copy/move/delete directories recursively, rename files and directories -- without installing any software." />
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="apple-touch-icon" href="favicon.png"/>
+<title>net2ftp - a web based FTP client</title>
+<?php net2ftp("printJavascript"); ?>
+<?php net2ftp("printCss"); ?>
 </head>
-<body>
-<?php if (get_magic_quotes_gpc()) { ?>
-    <div class="container">
-        <div class="grid12">
-            <p>
-                This PHP install has magic quotes enabled. This feature of PHP has been deprecated, and Monsta FTP is
-                not compatible with magic quotes.
-            </p>
-            <p>
-                Please disable PHP magic quotes to use Monsta FTP.
-            </p>
-            <p>
-                For more information, please see:
-                <a href="http://redirect.monstaftp.com/magic-quotes">http://redirect.monstaftp.com/magic-quotes</a>.
-            </p>
-        </div>
-    </div>
-<?php } else { ?>
-    <div id="spinner" ng-controller="SpinnerController" ng-show="spinnerVisible">
-        <div>
-            <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-        </div>
-    </div>
-    <div id="file-xfer-drop" ng-controller="DragDropController">
-        <div translate>DROP_FILES_INSTRUCTION</div>
-    </div>
-    <ng-include src="'application/frontend/templates/modal-chmod.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-login.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-password-management.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-editor.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-transfers.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-prompt.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-confirm.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-error.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-addons.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-settings.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-properties.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-login-link.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-choice.html'"></ng-include>
-    <ng-include src="'application/frontend/templates/modal-upgrade.html'"></ng-include>
+<body onload="<?php net2ftp("printBodyOnload"); ?>">
 
-    <div id="sb-site" canvas="container">
-        <ng-include src="'application/frontend/templates/body-header.html'"></ng-include>
-        <ng-include src="'application/frontend/templates/body-history.html'"></ng-include>
-        <ng-include src="'application/frontend/templates/body-files.html'"></ng-include>
-        <ng-include src="'application/frontend/templates/body-footer.html'"></ng-include>
-    </div>
+<?php net2ftp("printBody"); ?>
 
-    <ng-include src="'application/frontend/templates/body-slidebar.html'"></ng-include>
-    <iframe src="about:blank" id="download-iframe"></iframe>
-<?php } ?>
-    <script>
-        var versionQS = <?php print json_encode($versionQS); ?> + getFpQs();
-        document.write('<scri' + 'pt async src="//monstaftp.com/_callbacks/latest-version.php?' + versionQS + '"></scr' + 'ipt>')
-    </script>
+<?php
+// ------------------------------------------------------------------------
+// 4. Check the result and print out an error message. This can be done using 
+//    a template, or by accessing the $net2ftp_result variable directly.
+// ------------------------------------------------------------------------
+if ($net2ftp_result["success"] == false) {
+	require_once($net2ftp_globals["application_rootdir"] . "/skins/" . $net2ftp_globals["skin"] . "/error.template.php");
+}
+?>
+
 </body>
 </html>
